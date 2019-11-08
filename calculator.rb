17ce9cd614c232031f733e63d9c676ed1ba74a05
Sara Fotros <sarafotros1@yahoo.com>
2019-11-08 15:20:50 +0000

    Automatically backed up by Learn

--- calculator.rb
+++ calculator.rb
@@ -1,4 +1,9 @@
 first_number = 9.8
 second_number = 6
+sum = first_number + second_number
+difference = first_number - second_number
+product = first_numbe * second_number
+
+
 # Add your variables here
 
